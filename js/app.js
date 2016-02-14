a3dc30e4aff899e8e572f965dfa959f9c3a78788
katherineboliek <katherineboliek@gmail.com>
2016-02-14 13:20:41 -0500

    Connected json file, added a selector feature.

--- js/app.js
+++ js/app.js
@@ -3,5 +3,18 @@ var app = angular.module('myApp', []);
 app.controller('BaseController', function() {
 
     this.message = "Ready";
-
 });
+
+app.controller('CountryController', ['$http', function($http) {
+
+  this.countries = [];
+  var _this = this;
+
+  $http.get('/js/europe.json')
+    .success(function(data){
+      _this.countries = data;
+    })
+    .error(function(msg){
+      console.log("This request failed.\n");
+    });
+}]);
