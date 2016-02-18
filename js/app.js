c6d290295db04891d0f1083b2227f4cbb47cb1ca
katherineboliek <katherineboliek@gmail.com>
2016-02-18 10:23:33 -0500

    Added reverse sort functionality and cleaned up json data to repair previous bugs.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,11 @@ app.controller('CountryController', ['$http', function($http) {
   this.countries = [];
   this.activeCountry = "";
   var _this = this;
+  this.direction = true;
+
+  this.toggle = function(){
+    this.direction = !this.direction;
+  };
 
   $http.get('/js/europe.json')
     .success(function(data){
@@ -18,4 +23,5 @@ app.controller('CountryController', ['$http', function($http) {
     .error(function(msg){
       console.log("This request failed.\n");
     });
+
 }]);
